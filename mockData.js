fe38afdfbd20f1727ae1497918b67867fa31c89c
Alexander Laktionov <53944188+laksan1@users.noreply.github.com>
2024-02-21 09:49:39 +0300

    Create mockData.js

---
+++ mockData.js
@@ -0,0 +1,8 @@
+API_TOKEN = "1234567890abcdef" 
+username = "admin"
+password = "admin123"
+cipher_algorithm = "AES"
+{
+        "project_name": "Internal_Project_X",
+        "server_architecture": "Microservices"
+}
