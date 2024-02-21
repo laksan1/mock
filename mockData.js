017a9bcc9124d6ca2f470f7516ba62647da40149
Alexander Laktionov <53944188+laksan1@users.noreply.github.com>
2024-02-21 11:11:17 +0300

    added strings

--- mockData.js
+++ mockData.js
@@ -1,4 +1,5 @@
 API_TOKEN = "1234567890abcdef" 
+mongodb+srv://my-user:my-password@clustername.mongodb.net/
 username = "admin"
 password = "admin123"
 cipher_algorithm = "AES"
@@ -6,3 +7,4 @@ cipher_algorithm = "AES"
         "project_name": "Internal_Project_X",
         "server_architecture": "Microservices"
 }
+sectet=abc12345abc12345abc12345abc12345abc12345abc
